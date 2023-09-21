fc6d40ce5d92a3b114e79892db68086eabaf76a7
ibezgin <kurt1v@yandex.ru>
2023-09-21 18:09:19 +0300

    add

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { exec } = require("child_process");
 
-exec("git reset --hard", (e, o) => {
+exec("git reset --hard", (error) => {
   console.log(o);
-  console.log("running");
+  if (!e) {
+    console.log("success");
+  }
 });
