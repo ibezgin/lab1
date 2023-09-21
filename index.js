364a3421a6e3f1aa7e8d9300c6bbb4a1fd498c86
ibezgin <kurt1v@yandex.ru>
2023-09-21 18:43:14 +0300

    add

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ const { exec } = require("child_process");
 
 exec("git reset --hard", (error) => {
   if (!error) {
-    console.log("success");
+    console.log("success reseted");
   }
+});
+
+exec("git checkout production", (error) => {
+  if (!error) {
+    console.log("success checkout");
+  }
+  exec("git merge development", (error) => {
+    if (!error) {
+      console.log("success merged");
+    }
+  });
 });
